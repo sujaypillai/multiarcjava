4d8d9e04e8f4b089b3dc9daf33729ff91fc9768b
Sujay Pillai <sujayopillai@gmail.com>
2022-04-05 07:18:14 +0800

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 class hello{
     public static void main(String [] args){
-        System.out.println("Hello Docker ....");
+        System.out.println("Hello Docker from .... " + System.getProperty("os.arch"));
     }
 }
